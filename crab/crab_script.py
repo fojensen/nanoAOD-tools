1d5af06b208821e2641b8421ee3a1f2d15309450
Frank Jensen <frank.jensen@ucdavis.edu>
2020-06-27 14:04:58 -0500

    random etc before readme update

--- crab/crab_script.py
+++ crab/crab_script.py
@@ -1,7 +1,6 @@
 import datetime
-now = datetime.datetime.now()
-print ("Start date and time : ")
-print (now.strftime("%Y-%m-%d %H:%M:%S"))
+then = datetime.datetime.now()
+print ("Start date and time: ", then.strftime("%Y-%m-%d %H:%M:%S"))
 
 #!/usr/bin/env python
 import os
@@ -10,19 +9,20 @@ from PhysicsTools.NanoAODTools.postprocessing.framework.postprocessor import *
 #this takes care of converting the input files from CRAB
 from PhysicsTools.NanoAODTools.postprocessing.framework.crabhelper import inputFiles,runsAndLumis
 
-testfile = [
-#    "root://cmsxrootd.fnal.gov///store/data/Run2018D/SingleMuon/NANOAOD/Nano25Oct2019-v1/100000/76BEBA77-9DDB-8144-A183-5842729F006D.root"
-   "root://cmsxrootd.fnal.gov//store/mc/RunIIAutumn18NanoAODv6/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/NANOAODSIM/Nano25Oct2019_102X_upgrade2018_realistic_v20-v1/260000/E78C9017-BB6E-FE48-BA37-E059AEA79CD3.root"
-    #"root://cmsxrootd.fnal.gov//store/mc/RunIIAutumn18NanoAODv6/TTJets_TuneCP5_13TeV-madgraphMLM-pythia8/NANOAODSIM/Nano25Oct2019_102X_upgrade2018_realistic_v20-v1/250000/8969BAAF-2D11-7449-A180-97850997CD0A.root",
-#    "root://cmsxrootd.fnal.gov//store/mc/RunIIAutumn18NanoAODv6/WJetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8/NANOAODSIM/Nano25Oct2019_102X_upgrade2018_realistic_v20-v1/20000/E0FBA990-ABF5-3C4D-BCB3-9FCB6F0FFCB3.root" 
-    #"file:E0FBA990-ABF5-3C4D-BCB3-9FCB6F0FFCB3.root"
-]
+#testfile = [
+    #"root://cmsxrootd.fnal.gov///store/data/Run2018D/SingleMuon/NANOAOD/Nano25Oct2019-v1/100000/76BEBA77-9DDB-8144-A183-5842729F006D.root"
+    #"root://cmsxrootd.fnal.gov//store/mc/RunIIAutumn18NanoAODv6/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/NANOAODSIM/Nano25Oct2019_102X_upgrade2018_realistic_v20-v1/260000/C00024AD-3D0D-DE45-949F-E56A81BDDCA7.root" # small DY file
+    #"root://cmsxrootd.fnal.gov//store/mc/RunIIAutumn18NanoAODv6/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/NANOAODSIM/Nano25Oct2019_102X_upgrade2018_realistic_v20-v1/260000/109AFD23-2019-E447-8070-8FA4B9203018.root" # large DY file
+    #"root://cmsxrootd.fnal.gov//store/mc/RunIIAutumn18NanoAODv6/WJetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8/NANOAODSIM/Nano25Oct2019_102X_upgrade2018_realistic_v20-v1/20000/9E6A9BA6-C187-0F4E-8A45-01B2F2F33E11.root", #small W file
+    #"root://cmsxrootd.fnal.gov///store/mc/RunIIAutumn18NanoAODv6/WJetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8/NANOAODSIM/Nano25Oct2019_102X_upgrade2018_realistic_v20-v1/20000/E0FBA990-ABF5-3C4D-BCB3-9FCB6F0FFCB3.root" #large W file
+#]
 #print testfile
 
 cut_Mu = "(Sum$(Muon_pt>=27. && TMath::Abs(Muon_eta)<2.4 && Muon_tightId && Muon_pfIsoId>=4)>0)"
 cut_Trigger = "(HLT_IsoMu24||HLT_IsoMu27)"
 cut_Electron = "(Sum$(Electron_pt>=20. && TMath::Abs(Electron_eta)<2.5 && Electron_mvaFall17V1Iso_WPL)==0)"
 cut_Tau = "(Sum$(Tau_pt>=20. && TMath::Abs(Tau_eta)<2.3 && (8&Tau_idDeepTau2017v2p1VSmu) && (128&Tau_idDeepTau2017v2p1VSe) && !(Tau_decayMode==5||Tau_decayMode==6))>0)"
+#https://twiki.cern.ch/CMS/MissingETOptionalFiltersRun2#2018_data
 cut_Flag = "(Flag_goodVertices && Flag_globalSuperTightHalo2016Filter && Flag_HBHENoiseFilter && Flag_HBHENoiseIsoFilter && Flag_EcalDeadCellTriggerPrimitiveFilter && Flag_BadPFMuonFilter)"
 cut_ = cut_Mu + " && " + cut_Trigger + " && " + cut_Electron + " && " + cut_Tau + " && " + cut_Flag
 print cut_
@@ -38,20 +38,21 @@ modules_ = [MuTauProducerConstr(), MuMuProducerConstr()]
 
 p=PostProcessor(
     outputDir = "./",
-#    inputFiles = inputFiles(),
-    inputFiles = testfile,
+    inputFiles = inputFiles(),
+    #inputFiles = testfile,
     cut = cut_,
     modules = modules_,
     #maxEntries = 100000,
     provenance = True,
     fwkJobReport = True,
-    #haddFileName = "DYJetsToLL_M-50.root",
+    #haddFileName = "test.root",
     jsonInput = runsAndLumis(),
     outputbranchsel = "keep_and_drop.txt"
 )
 p.run()
 
-then = datetime.datetime.now()
-print ("Finish date and time : ")
-print (then.strftime("%Y-%m-%d %H:%M:%S"))
+now = datetime.datetime.now()
+print ("Finish date and time: ", now.strftime("%Y-%m-%d %H:%M:%S"))
+duration = now - then
+print ("Total seconds elapsed: ", duration.total_seconds())
 
